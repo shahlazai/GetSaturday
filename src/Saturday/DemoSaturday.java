315f08683d5e32e7e6904342b9d08d9ae572df94
shahl <shahl@192.168.1.239>
2021-10-02 10:22:49 -0400

    both must push

---
+++ src/Saturday/DemoSaturday.java
@@ -0,0 +1,9 @@
+package Saturday;
+
+public class DemoSaturday {
+	
+	public static void main(String[]args) {
+		System.out.println("Saturday Demo....");
+	}
+
+}
---
+++ src/Saturday/DemoSunday.java
@@ -0,0 +1,12 @@
+package Saturday;
+
+public class DemoSunday {
+	
+	public static void main(String[]args) {
+		System.out.println("Sunday Demo....");
+	}
+
+}
+
+
+
